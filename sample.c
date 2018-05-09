c65bfea89ba588e091c1ea0d6b9b0c7c4aaacedd
andrade94 <eduardo.mtz9@gmail.com>
2018-05-09 02:10:23 -0500

    Cube with colors.

--- sample.c
+++ sample.c
@@ -19,159 +19,122 @@
 #endif
  
 #include <stdlib.h>
- 
-static int slices = 16;
-static int stacks = 16;
- 
-/* GLUT callback Handlers */
- 
-static void resize( int width, int height)
-{
-     const float ar = ( float ) width / ( float ) height;
- 
-     glViewport(0, 0, width, height);
-     glMatrixMode(GL_PROJECTION);
-     glLoadIdentity();
-     glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
- 
-     glMatrixMode(GL_MODELVIEW);
-     glLoadIdentity() ;
+
+/* Global variables */
+char title[] = "3D Shapes with animation";
+GLfloat angleCube = 0.0f;     // Rotational angle for cube [NEW]
+int refreshMills = 15;        // refresh interval in milliseconds [NEW]
+
+/* Initialize OpenGL Graphics */
+void initGL() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+    glClearDepth(1.0f);                   // Set background depth to farthest
+    glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+    glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
+    glShadeModel(GL_SMOOTH);   // Enable smooth shading
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
 }
- 
-static void display( void )
-{
-     const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-     const double a = t*90.0;
- 
-     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-     glColor3d(1,0,0);
- 
-     glPushMatrix();
-         glTranslated(-2.4,1.2,-6);
-         glRotated(60,1,0,0);
-         glRotated(a,0,0,1);
-         glutSolidSphere(1,slices,stacks);
-     glPopMatrix();
- 
-     glPushMatrix();
-         glTranslated(0,1.2,-6);
-         glRotated(60,1,0,0);
-         glRotated(a,0,0,1);
-         glutSolidCone(1,1,slices,stacks);
-     glPopMatrix();
- 
-     glPushMatrix();
-         glTranslated(2.4,1.2,-6);
-         glRotated(60,1,0,0);
-         glRotated(a,0,0,1);
-         glutSolidTorus(0.2,0.8,slices,stacks);
-     glPopMatrix();
- 
-     glPushMatrix();
-         glTranslated(-2.4,-1.2,-6);
-         glRotated(60,1,0,0);
-         glRotated(a,0,0,1);
-         glutWireSphere(1,slices,stacks);
-     glPopMatrix();
- 
-     glPushMatrix();
-         glTranslated(0,-1.2,-6);
-         glRotated(60,1,0,0);
-         glRotated(a,0,0,1);
-         glutWireCone(1,1,slices,stacks);
-     glPopMatrix();
- 
-     glPushMatrix();
-         glTranslated(2.4,-1.2,-6);
-         glRotated(60,1,0,0);
-         glRotated(a,0,0,1);
-         glutWireTorus(0.2,0.8,slices,stacks);
-     glPopMatrix();
- 
-     glutSwapBuffers();
+
+/* Handler for window-repaint event. Called back when the window first appears and
+ whenever the window needs to be re-painted. */
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
+    glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+    
+    // Render a color-cube consisting of 6 quads with different colors
+    glLoadIdentity();                 // Reset the model-view matrix
+    glTranslatef(1.5f, 0.0f, -7.0f);  // Move right and into the screen
+    glRotatef(angleCube, 1.0f, 1.0f, 1.0f);  // Rotate about (1,1,1)-axis [NEW]
+    
+    glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+    // Top face (y = 1.0f)
+    // Define vertices in counter-clockwise (CCW) order with normal pointing out
+    glColor3f(0.0f, 1.0f, 0.0f);     // Green
+    glVertex3f( 1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f,  1.0f);
+    glVertex3f( 1.0f, 1.0f,  1.0f);
+    
+    // Bottom face (y = -1.0f)
+    glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+    glVertex3f( 1.0f, -1.0f,  1.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f( 1.0f, -1.0f, -1.0f);
+    
+    // Front face  (z = 1.0f)
+    glColor3f(1.0f, 0.0f, 0.0f);     // Red
+    glVertex3f( 1.0f,  1.0f, 1.0f);
+    glVertex3f(-1.0f,  1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f( 1.0f, -1.0f, 1.0f);
+    
+    // Back face (z = -1.0f)
+    glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+    glVertex3f( 1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f,  1.0f, -1.0f);
+    glVertex3f( 1.0f,  1.0f, -1.0f);
+    
+    // Left face (x = -1.0f)
+    glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+    glVertex3f(-1.0f,  1.0f,  1.0f);
+    glVertex3f(-1.0f,  1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);
+    
+    // Right face (x = 1.0f)
+    glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+    glVertex3f(1.0f,  1.0f, -1.0f);
+    glVertex3f(1.0f,  1.0f,  1.0f);
+    glVertex3f(1.0f, -1.0f,  1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glEnd();  // End of drawing color-cube
+    
+    // Render a pyramid consists of 4 triangles
+    glLoadIdentity();                  // Reset the model-view matrix
+    glTranslatef(-1.5f, 0.0f, -3.0f);  // Move left and into the screen
+
+    glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
+    
+    // Update the rotational angle after each refresh [NEW]
+    angleCube -= 0.15f;
 }
- 
- 
-static void key(unsigned char key, int x, int y)
-{
-     switch (key)
-     {
-         case 27 :
-         case 'q' :
-             exit (0);
-             break ;
- 
-         case '+' :
-             slices++;
-             stacks++;
-             break ;
- 
-         case '-' :
-             if (slices>3 && stacks>3)
-             {
-                 slices--;
-                 stacks--;
-             }
-             break ;
-     }
- 
-     glutPostRedisplay();
+
+/* Called back when timer expired [NEW] */
+void timer(int value) {
+    glutPostRedisplay();      // Post re-paint request to activate display()
+    glutTimerFunc(refreshMills, timer, 0); // next timer call milliseconds later
 }
- 
-static void idle( void )
-{
-     glutPostRedisplay();
+
+/* Handler for window re-size event. Called back when the window first appears and
+ whenever the window is re-sized with its new width and height */
+void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
+    // Compute aspect ratio of the new window
+    if (height == 0) height = 1;                // To prevent divide by 0
+    GLfloat aspect = (GLfloat)width / (GLfloat)height;
+    
+    // Set the viewport to cover the new window
+    glViewport(0, 0, width, height);
+    
+    // Set the aspect ratio of the clipping volume to match the viewport
+    glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+    glLoadIdentity();             // Reset
+    // Enable perspective projection with fovy, aspect, zNear and zFar
+    gluPerspective(45.0f, aspect, 0.1f, 100.0f);
 }
- 
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
- 
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
- 
-/* Program entry point */
- 
-int main( int argc, char *argv[])
-{
-     glutInit(&argc, argv);
-     glutInitWindowSize(640,480);
-     glutInitWindowPosition(10,10);
-     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
- 
-     glutCreateWindow( "GLUT Shapes" );
- 
-     glutReshapeFunc(resize);
-     glutDisplayFunc(display);
-     glutKeyboardFunc(key);
-     glutIdleFunc(idle);
- 
-     glClearColor(1,1,1,1);
-     glEnable(GL_CULL_FACE);
-     glCullFace(GL_BACK);
- 
-     glEnable(GL_DEPTH_TEST);
-     glDepthFunc(GL_LESS);
- 
-     glEnable(GL_LIGHT0);
-     glEnable(GL_NORMALIZE);
-     glEnable(GL_COLOR_MATERIAL);
-     glEnable(GL_LIGHTING);
- 
-     glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-     glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
- 
-     glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-     glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-     glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-     glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
- 
-     glutMainLoop();
- 
-     return EXIT_SUCCESS;
+
+/* Main function: GLUT runs as a console application starting at main() */
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);            // Initialize GLUT
+    glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
+    glutInitWindowSize(640, 480);   // Set the window's initial width & height
+    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+    glutCreateWindow(title);          // Create window with the given title
+    glutDisplayFunc(display);       // Register callback handler for window re-paint event
+    glutReshapeFunc(reshape);       // Register callback handler for window re-size event
+    initGL();                       // Our own OpenGL initialization
+    glutTimerFunc(0, timer, 0);     // First timer call immediately [NEW]
+    glutMainLoop();                 // Enter the infinite event-processing loop
+    return 0;
 }
